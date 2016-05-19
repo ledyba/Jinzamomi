8d0d2424c58aa75aada27c3f4a443a780b55f555
Kaede Fujisaki <kaede@hexe.net>
2016-05-19 21:57:08 +0900

    update language-krkr

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,32 @@ import qualified Data.Text as T
 import Language.TJS as TJS
 import Jinzamomi.Driver.Krkr.TJS2IR as TJS2IR
 import Jinzamomi.Driver.IR as IR
+import System.Log.Logger
+import System.Log.Handler.Syslog
+import System.Log.Handler.Simple
+import System.Log.Handler (setFormatter)
+import System.Log.Formatter
+import GHC.IO.Handle.FD as FD
+
+compile :: FilePath -> FilePath -> IO ()
+compile from to = do
+  file <- readFile from
+  case TJS.parse from (T.pack file) of
+    Right ast -> do
+      let out = T.unpack (IR.compile (TJS2IR.compile ast))
+      writeFile to out
+      infoM "Krkr" $ "Compiled source:\n" ++ out
+    Left err -> error (show err)
+
+setupLogger :: Priority -> IO ()
+setupLogger level = do
+  logger <- getRootLogger
+  handler <-  verboseStreamHandler FD.stdout DEBUG
+  let handler' = setFormatter handler (simpleLogFormatter "[$time][$prio@$loggername] $msg")
+  saveGlobalLogger $ setLevel level $ addHandler handler' $ removeHandler logger
 
 main :: IO ()
 main = do
-  file <- readFile "ext/kag3/data/startup.tjs"
-  case TJS.parse "startup.tjs" (T.pack file) of
-    Right ast -> do
-      let out = T.unpack (IR.compile (TJS2IR.compile ast))
-      writeFile "runtime/test/proj/startup.js" out
-      putStrLn out
-    Left err -> error (show err)
+  setupLogger INFO
+  Main.compile "ext/kag3/data/startup.tjs" "runtime/test/proj/startup.js"
+  Main.compile "ext/kag3/data/system/Initialize.tjs" "runtime/test/proj/system/Initialize.js"
--- test/IRSpec.hs
+++ test/IRSpec.hs
@@ -2,13 +2,13 @@
 module IRSpec (spec) where
 
 import Test.Hspec
-import Uzume.Driver.IR
+import Jinzamomi.Driver.IR
 
 spec :: Spec
 spec =
   describe "basic test" $ do
     it "value test" $ do
       compile (Var "test") `shouldBe` "test;"
-      compile (Dot (Var "test") "value") `shouldBe` "test.value;"
+      compile (Dot (Var "test") "value") `shouldBe` "test[\"value\"];"
       compile (Block [Var "test"]) `shouldBe` "{\n  test;\n}"
       compile (Try (Block [Var "test"]) "e" (Block [Var "test2"])) `shouldBe` "try\n{\n  test;\n}\ncatch(e)\n{\n  test2;\n}"
