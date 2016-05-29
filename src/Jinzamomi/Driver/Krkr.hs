c5a03d676781318cea7bef1c5d7e14a7f2ca1835
Kaede Fujisaki <kaede@hexe.net>
2016-05-29 18:38:06 +0900

    implement builder

--- src/Jinzamomi/Driver/Krkr.hs
+++ src/Jinzamomi/Driver/Krkr.hs
@@ -5,37 +5,61 @@ module Jinzamomi.Driver.Krkr (
 ) where
 
 import qualified Data.Text as T
-import Language.TJS as TJS
+import qualified Data.Text.IO as TIO
+import qualified　Language.TJS as TJS
 import System.Log.Logger
 import Options.Applicative
-import Jinzamomi.Driver.Krkr.TJS2IR as TJS2IR
-import Jinzamomi.Driver.IR as IR
+import qualified Jinzamomi.Driver.Krkr.TJS2IR as TJS2IR
+import qualified Jinzamomi.Driver.IR as IR
 import Jinzamomi.Driver.Util
+import System.FilePath (dropFileName, takeExtension, (</>))
+import System.Directory (createDirectoryIfMissing)
 
 data Opt =
     Build String String
   deriving (Show)
 
+tag :: String
+tag = "Krkr"
+
 opt :: Mod CommandFields Opt
 opt = command "krkr" (info buildCmd (progDesc "Krkr Driver."))
   where
-    buildCmd = hsubparser $ command "build" (info buildOption (progDesc "Build krkr"))
-    buildOption = Build <$> argument str (metavar "FROM") <*> argument str (metavar "TO")
+    buildCmd = hsubparser $
+      command "build" (
+        info buildOption
+        (progDesc "Build krkr"))
+    buildOption = Build <$>
+            argument str (metavar "FROM") <*>
+            argument str (metavar "TO")
 
-compile :: FilePath -> FilePath -> IO ()
-compile from to = do
-  file <- readFile from
-  case TJS.parse from (T.pack file) of
+compile :: FilePath -> FilePath -> FilePath -> IO Bool
+compile from to src = do
+  file <- TIO.readFile (from </> src)
+  let outTo = to </> src
+  createDirectoryIfMissing True (dropFileName outTo)
+  case TJS.parse src file of
     Right ast -> do
-      let out = T.unpack (IR.compile (TJS2IR.compile ast))
-      writeFile to out
-      infoM "Krkr" $ "Compiled source:\n" ++ out
-    Left err -> error (show err)
+      let out = IR.compile (TJS2IR.compile ast)
+      infoM tag ("TJS2 Compile: " ++ src)
+      TIO.writeFile outTo out
+      return True
+    Left err -> do
+      errorM tag ("Error while compiling: " ++ src)
+      mapM_ (errorM tag) (lines (show err))
+      return False
 
 build :: String -> String -> IO ()
 build from to = do
+  createDirectoryIfMissing True to
   files <- enumAllFiles from
-  mapM_ putStrLn files
+  let tjsFiles = filter (\f -> takeExtension f == ".tjs") files
+  results <- mapM (compile from to) tjsFiles
+  let errors = length (filter not results)
+  if errors /= 0 then
+    errorM tag (concat ["compiling errors on ", show errors, "(/", show (length results), ") tjs2 files. done."])
+  else
+    return ()
 
 execute :: Opt -> IO ()
 execute (Build from to) = build from to
--- src/Jinzamomi/Driver/Util.hs
+++ src/Jinzamomi/Driver/Util.hs
@@ -4,16 +4,21 @@ module Jinzamomi.Driver.Util (
 --
 
 import System.Directory
-import System.FilePath (pathSeparator)
 import Control.Monad (filterM)
 import Data.List.Utils (endswith)
+import System.FilePath (pathSeparator, makeRelative, normalise)
 
 --
 enumAllFiles :: FilePath -> IO [FilePath]
 enumAllFiles path = do
-    allItems <- getDirectoryContents path
-    let items = (\f -> path ++ pathSeparator:f) <$> filter (\k -> k /= "." && k /= "..") allItems
-    files <- filterM doesFileExist items
-    justFolders <- filterM doesDirectoryExist items
-    leftFiles <- mapM enumAllFiles justFolders
-    return (files ++ concat leftFiles)
+    files <- enumAllFiles' path
+    return (fmap (makeRelative path) files)
+  where
+    enumAllFiles' path = do
+      let normpath = normalise path
+      allItems <- getDirectoryContents path
+      let items = (\f -> path ++ pathSeparator:f) <$> filter (\k -> k /= "." && k /= "..") allItems
+      files <- filterM doesFileExist items
+      justFolders <- filterM doesDirectoryExist items
+      leftFiles <- mapM enumAllFiles' justFolders
+      return (files ++ concat leftFiles)
