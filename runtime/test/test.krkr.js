479011883d321ef998d7d1f4499dc89da434f614
Kaede Fujisaki <kaede@hexe.net>
2016-10-27 14:55:17 +0900

    add some basic test.

--- runtime/test/test.krkr.js
+++ runtime/test/test.krkr.js
@@ -1,4 +1,9 @@
 describe('ArraySuite', () => {
-  it('my test', () => {
+  var jArray = jinzamomi.krkr.global.Array;
+  it('create test', () => {
+    var arr = new jArray();
+    expect(arr).to.not.be(null);
+    expect(arr).to.be.a(jArray);
+    expect(arr.count).to.eql(0);
   });
 });
